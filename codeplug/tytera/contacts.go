d6267003ca20f1e13db36bbd328cd192bb11b27c
Jesus Trujillo <jesusft@oath.com>
2018-06-13 19:24:37 -0700

    parsing contacts

---
+++ codeplug/tytera/contacts.go
@@ -0,0 +1,128 @@
+package tytera
+
+import (
+	"fmt"
+	"github.com/yeyus/dmr-codeplug/encoding"
+	"github.com/yeyus/dmr-codeplug/encoding/base"
+	"github.com/yeyus/dmr-codeplug/proto/tytera"
+	"go/types"
+)
+
+type Contacts struct {
+	EntityID string
+	Base     uint32
+	Length   uint32
+	Decoders []encoding.Decoder
+	Contacts tytera.Contacts
+}
+
+func GetContacts() Contacts {
+	m := Contacts{
+		EntityID: "com.tytera.contacts",
+		Base:     0x6080,
+		Length:   0x8CA0,
+		Contacts: tytera.Contacts{},
+	}
+
+	m.Decoders = []encoding.Decoder{
+		&encoding.RepeatedDecoder{
+			Offset:       0,
+			RecordLength: 0x24,
+			Records:      1000,
+			Decoder:      GetContactEntryDecoder(),
+		},
+	}
+
+	return m
+}
+
+func (t *Contacts) Decode(buf []byte, base uint32) (m map[string]string) {
+	m = map[string]string{}
+
+	d := t.Decoders[0]
+	value := d.Decode(buf, base+t.Base)
+
+	var arr []*tytera.ContactEntry
+	for k, v := range value.([]interface{}) {
+		entry := v.(tytera.ContactEntry)
+		arr = append(arr, &entry)
+		m[fmt.Sprintf(d.GetEntityID(), k)] = fmt.Sprintf("%+v", entry)
+	}
+
+	t.Contacts.Entries = arr
+
+	return
+}
+
+type ContactEntryDecoder struct {
+	EntityID string
+	Decoders []encoding.Decoder
+	Entry    tytera.ContactEntry
+}
+
+func GetContactEntryDecoder() ContactEntryDecoder {
+	m := ContactEntryDecoder{
+		EntityID: "com.tytera.contacts[%d]",
+		Entry:    tytera.ContactEntry{},
+	}
+
+	m.Decoders = []encoding.Decoder{
+		&base.Uint64Decoder{
+			EntityID:  "id",
+			Offset:    0,
+			Length:    3,
+			Endianess: base.BigEndian,
+		},
+		&base.BitMaskDecoder{
+			EntityID: "callType",
+			Offset:   3,
+			BitMask:  0x0F,
+		},
+		&base.BitDecoder{
+			EntityID:  "callReceiveTone",
+			Offset:    3,
+			BitOffset: 5,
+		},
+		&base.UTF16StringDecoder{
+			EntityID:  "name",
+			Offset:    4,
+			Length:    32,
+			Endianess: base.LittleEndian,
+		},
+	}
+
+	return m
+}
+
+func (t ContactEntryDecoder) GetEntityID() string {
+	return t.EntityID
+}
+
+func (t ContactEntryDecoder) GetEntityType() types.BasicKind {
+	return types.UnsafePointer
+}
+
+func (t ContactEntryDecoder) Decode(buf []byte, base uint32) interface{} {
+	for _, d := range t.Decoders {
+		t.mapValue(d, buf, base)
+	}
+
+	return t.Entry
+}
+
+func (t *ContactEntryDecoder) mapValue(d encoding.Decoder, buf []byte, base uint32) {
+	switch id := d.GetEntityID(); id {
+	case "id":
+		s := d.Decode(buf, base).(uint64)
+		t.Entry.Id = uint32(s)
+	case "callType":
+		s := d.Decode(buf, base).(uint8)
+		t.Entry.CallType = tytera.CallType(uint32(s))
+	case "callReceiveTone":
+		s := d.Decode(buf, base).(bool)
+		t.Entry.CallReceiveTone = s
+	case "name":
+		s := d.Decode(buf, base).(string)
+		t.Entry.Name = string(s)
+	}
+}
---
+++ codeplug/tytera/contacts_test.go
@@ -0,0 +1,71 @@
+package tytera
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/yeyus/dmr-codeplug/proto/tytera"
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+type contactsTest struct{}
+
+func (contactsTest) getRDTBytes(file string) []byte {
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return content
+}
+
+func TestContactsParsing(t *testing.T) {
+	d := contactsTest{}
+	content := d.getRDTBytes("../../packing/tytera/testdata/usa_codeplug.rdt")
+
+	cs := GetContacts()
+
+	b, err := json.MarshalIndent(cs.Decode(content[:], 0x125), "", " ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Print(string(b))
+	fmt.Printf("%+v", cs.Contacts)
+}
+
+func TestContactsProto(t *testing.T) {
+	d := contactsTest{}
+	content := d.getRDTBytes("../../packing/tytera/testdata/usa_codeplug.rdt")
+
+	cs := GetContacts()
+	cs.Decode(content[:], 0x125)
+
+	testContact(t, cs, 0, 2, "Local", true, tytera.CallType_GROUP_CALL)
+	testContact(t, cs, 1, 9, "Hotspot", false, tytera.CallType_PRIVATE_CALL)
+	testContact(t, cs, 2, 0xFFFFFF, "Worldwide", false, tytera.CallType_ALL_CALL)
+	testContact(t, cs, 3, 93, "North America", false, tytera.CallType_GROUP_CALL)
+	testContact(t, cs, 40, 3139, "Ohio", false, tytera.CallType_GROUP_CALL)
+	testContact(t, cs, 77, 312, "TAC 312", false, tytera.CallType_GROUP_CALL)
+
+}
+
+func testContact(t *testing.T, c Contacts, idx int, id uint32, name string, callTone bool, callType tytera.CallType) {
+	entry := c.Contacts.Entries[idx]
+	if entry.Id != id {
+		t.Errorf("[idx %d] expected contact id to be %d, but got %d", idx, id, entry.Id)
+	}
+
+	if entry.Name != name {
+		t.Errorf("[idx %d] expected contact name to be %s, but got %s", idx, name, entry.Name)
+	}
+
+	if entry.CallReceiveTone != callTone {
+		t.Errorf("[idx %d] expected call tone to be %t, but got %t", idx, entry.CallReceiveTone, callTone)
+	}
+
+	if entry.CallType != callType {
+		t.Errorf("[idx %d] expected call type to be %s, but got %s", idx, entry.CallType, callType)
+	}
+}
