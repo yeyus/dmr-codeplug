d05c73e81ed5c9e61163a58819b33b2117d1dd9d
Jesus Trujillo <jesusft@oath.com>
2018-06-24 00:07:29 -0700

    add parsing for scan lists

---
+++ codeplug/tytera/scan_lists.go
@@ -0,0 +1,163 @@
+package tytera
+
+import (
+	"fmt"
+	"github.com/yeyus/dmr-codeplug/encoding"
+	"github.com/yeyus/dmr-codeplug/encoding/base"
+	"github.com/yeyus/dmr-codeplug/proto/tytera"
+	"go/types"
+)
+
+type ScanListGroup struct {
+	EntityID  string
+	Base      uint32
+	Length    uint32
+	Decoders  []encoding.Decoder
+	ScanLists tytera.ScanLists
+}
+
+func GetScanListGroup() ScanListGroup {
+	m := ScanListGroup{
+		EntityID:  "com.tytera.scanList",
+		Base:      0x18960,
+		Length:    0x6590,
+		ScanLists: tytera.ScanLists{},
+	}
+
+	m.Decoders = []encoding.Decoder{
+		&encoding.RepeatedDecoder{
+			Offset:       0,
+			RecordLength: 0x68,
+			Records:      250,
+			Decoder:      GetScanListEntryDecoder(),
+		},
+	}
+
+	return m
+}
+
+func (t *ScanListGroup) Decode(buf []byte, base uint32) (m map[string]string) {
+	m = map[string]string{}
+
+	d := t.Decoders[0]
+	value := d.Decode(buf, base+t.Base)
+
+	var arr []*tytera.ScanListEntry
+	for k, v := range value.([]interface{}) {
+		entry := v.(tytera.ScanListEntry)
+		arr = append(arr, &entry)
+		m[fmt.Sprintf(d.GetEntityID(), k)] = fmt.Sprintf("%+v", entry)
+	}
+
+	t.ScanLists.Entries = arr
+
+	return
+}
+
+type ScanListEntryDecoder struct {
+	EntityID string
+	Decoders []encoding.Decoder
+	Entry    tytera.ScanListEntry
+}
+
+func GetScanListEntryDecoder() ScanListEntryDecoder {
+	m := ScanListEntryDecoder{
+		EntityID: "com.tytera.scanList[%d]",
+		Entry:    tytera.ScanListEntry{},
+	}
+
+	m.Decoders = []encoding.Decoder{
+		&base.UTF16StringDecoder{
+			EntityID:  "name",
+			Offset:    0,
+			Length:    32,
+			Endianess: base.LittleEndian,
+		},
+		&base.Uint64Decoder{
+			EntityID:  "priorityChannel1",
+			Offset:    32,
+			Length:    2,
+			Endianess: base.BigEndian,
+		},
+		&base.Uint64Decoder{
+			EntityID:  "priorityChannel2",
+			Offset:    34,
+			Length:    2,
+			Endianess: base.BigEndian,
+		},
+		&base.Uint64Decoder{
+			EntityID:  "txChannel",
+			Offset:    36,
+			Length:    2,
+			Endianess: base.BigEndian,
+		},
+		&base.ByteDecoder{
+			EntityID: "signalHoldTime",
+			Offset:   39,
+		},
+		&base.ByteDecoder{
+			EntityID: "prioritySampleTime",
+			Offset:   40,
+		},
+		&encoding.RepeatedDecoder{
+			Offset:       42,
+			RecordLength: 2,
+			Records:      31,
+			Decoder: &base.Uint64Decoder{
+				EntityID:  "channelIndex[%d]",
+				Offset:    0,
+				Length:    2,
+				Endianess: base.BigEndian,
+			},
+		},
+	}
+
+	return m
+}
+
+func (t ScanListEntryDecoder) GetEntityID() string {
+	return t.EntityID
+}
+
+func (t ScanListEntryDecoder) GetEntityType() types.BasicKind {
+	return types.UnsafePointer
+}
+
+func (t ScanListEntryDecoder) Decode(buf []byte, base uint32) interface{} {
+	for _, d := range t.Decoders {
+		t.mapValue(d, buf, base)
+	}
+
+	return t.Entry
+}
+
+func (t *ScanListEntryDecoder) mapValue(d encoding.Decoder, buf []byte, base uint32) {
+	switch id := d.GetEntityID(); id {
+	case "name":
+		s := d.Decode(buf, base).(string)
+		t.Entry.Name = string(s)
+	case "priorityChannel1":
+		s := d.Decode(buf, base).(uint64)
+		t.Entry.PriorityChannel_1 = uint32(s)
+	case "priorityChannel2":
+		s := d.Decode(buf, base).(uint64)
+		t.Entry.PriorityChannel_2 = uint32(s)
+	case "txChannel":
+		s := d.Decode(buf, base).(uint64)
+		t.Entry.TxChannel = uint32(s)
+	case "signalHoldTime":
+		s := d.Decode(buf, base).(uint8)
+		t.Entry.SignalHoldTime = uint32(s) * 25
+	case "prioritySampleTime":
+		s := d.Decode(buf, base).(uint8)
+		t.Entry.PrioritySampleTime = uint32(s) * 250
+	case "channelIndex[%d]":
+		s := d.Decode(buf, base).([]interface{})
+		var arr []uint32
+		for _, v := range s {
+			arr = append(arr, uint32(v.(uint64)))
+		}
+
+		t.Entry.ChannelIndex = arr
+	}
+}
